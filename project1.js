cd125dce8cf51c1378431a18aee7060ac38af3ad
Rohkyoungjin <rudwls468@gmail.com>
2020-03-02 18:01:33 +0900

    이벤트 2개 완성
    두번째 이벤트 보완해야함

---
+++ project1.js
@@ -0,0 +1,6 @@
+//alert("Welcome to Double Slash HW1 Homepage!");
+var content = document.querySelector('.mainContent');
+
+function btnOnClick(){
+    content.innerHTML = '<ol>Name: 노경진</ol><ol>Age: 24살</ol><ol>Github: <a href="https://github.com/kyoung-jnn">https://github.com/kyoung-jnn</a></ol>'
+}
