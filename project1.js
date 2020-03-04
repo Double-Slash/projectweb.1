b8fbe6ddcca32b0d93f04de4ab726e442a054fde
hwangbo <taekwonod714@naver.com>
2020-03-04 20:10:24 +0900

    feat : ajax 코드 구현

--- project1.js
+++ project1.js
@@ -11,3 +11,42 @@ function button_toggle() {
         });
     }
 }
+
+function getData() {
+    // data = {
+    //     img : ./flower.png,
+    //     list : [...],
+    //     video : ./movie.mp4,
+    //     words : "..."
+    // }
+    // 임을 가정
+    $(document).ready(function () {
+        $.ajax({
+            url: "", // TODO
+            type: 'GET',
+            cache: false,
+            success: function (data) {
+                document.getElementById('#image').src = data.img;
+                document.getElementById('#video').src = data.video;
+
+                var word = "";
+                word += "<p>";
+                word += data.words;
+                word += "</p>";
+                $("words").append(word);
+
+                var list = "<ul>";
+                for (var a in data.list) {
+                    list += "<li>";
+                    list += a;
+                    list += "</li>";
+                }
+                list += "</ul>";
+                $("#list").append(list);
+            },
+            error: function (data) {
+                alert("404. Please wait until the File is Loaded.");
+            }
+        })
+    })
+}
